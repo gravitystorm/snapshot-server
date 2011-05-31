59602327b503f719bdc56ecc0be1e9abb142ae63
Andy Allan <andy@gravitystorm.co.uk>
2011-05-31 11:58:11 +0100

    New project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BackgroundServer::Application.config.secret_token = 'a059780d3a7424411af4d14220cd9837000412b0bb9706cf49e3ee67a8b0220dbb4aa96090e718b9dd501737a6e08c5d5a936f039cbb7d43c8b041e28c003710'
