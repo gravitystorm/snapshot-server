5d04cf3ffcd5e5348040060986fafe84a510e5ea
Andy Allan <andy@gravitystorm.co.uk>
2011-06-17 17:49:26 +0100

    Add a mechanism for changing the status of a way

--- config/routes.rb
+++ config/routes.rb
@@ -56,6 +56,8 @@ SnapshotServer::Application.routes.draw do
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id(.:format)))'
 
+  match 'api/way/:id/status' => 'way#status', :via => :post
   match 'api/map' => 'api#map'
   match 'node/:id' => 'node#read'
+
 end
