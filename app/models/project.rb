3b501a6b9212e268c07a75c6bc0d2d1f678a9b1a
Andy Allan <andy@gravitystorm.co.uk>
2012-01-17 15:03:35 +0000

    Remove the project_ prefix from models.

--- app/models/project.rb
+++ app/models/project.rb
@@ -10,47 +10,49 @@
 #
 
 class Project < ActiveRecord::Base
-  has_many :project_nodes
-  has_many :project_relations
-  has_many :project_relation_members
-  has_many :project_way_nodes
-  has_many :project_ways
-  has_many :project_users
+
+  validates :title, :presence => true, :uniqueness => true
+  has_many :nodes, :class_name => "ProjectNode"
+  has_many :relations, :class_name => "ProjectRelation"
+  has_many :relation_members, :class_name => "ProjectRelationMember"
+  has_many :way_nodes, :class_name => "ProjectWayNode"
+  has_many :ways, :class_name => "ProjectWay"
+  has_many :users, :class_name => "ProjectUsers"
 
   # This might take a while...
   def transfer
     Node.all.each do |node|
-      pn = self.project_nodes.new
+      pn = self.nodes.new
       pn.update_from(node)
       pn.save!
     end
 
     Relation.all.each do |relation|
-      pr = self.project_relations.new
+      pr = self.relations.new
       pr.update_from(relation)
       pr.save!
     end
 
     RelationMember.all.each do |relation_member|
-      prm = self.project_relation_members.new
+      prm = self.relation_members.new
       prm.update_from(relation_member)
       prm.save!
     end
 
     WayNode.all.each do |way_node|
-      pwn = self.project_way_nodes.new
+      pwn = self.way_nodes.new
       pwn.update_from(way_node)
       pwn.save!
     end
 
     Way.all.each do |way|
-      pw = self.project_ways.new
+      pw = self.ways.new
       pw.update_from(way)
       pw.save!
     end
 
     User.all.each do |user|
-      pu = self.project_users.new
+      pu = self.users.new
       pu.update_from(user)
       pu.save!
     end
