24a53381f8f57e701f02db3ccf6745ee0a3353d2
Andy Allan <andy@gravitystorm.co.uk>
2012-01-19 11:23:01 +0000

    annotate

--- spec/factories/project_relations.rb
+++ spec/factories/project_relations.rb
@@ -1,3 +1,18 @@
+# == Schema Information
+#
+# Table name: project_relations
+#
+#  id           :integer         not null, primary key
+#  project_id   :integer         not null
+#  osm_id       :integer(8)      not null
+#  version      :integer         not null
+#  user_id      :integer         not null
+#  tstamp       :datetime        not null
+#  changeset_id :integer(8)      not null
+#  tags         :hstore
+#  status       :text
+#
+
 FactoryGirl.define do
   factory :project_relation do
     project_id 1
--- spec/factories/project_way_nodes.rb
+++ spec/factories/project_way_nodes.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: project_way_nodes
+#
+#  id          :integer         not null, primary key
+#  project_id  :integer         not null
+#  way_id      :integer(8)      not null
+#  node_id     :integer(8)      not null
+#  sequence_id :integer         not null
+#
+
 FactoryGirl.define do
   factory :project_way_node do
     project_id 1
