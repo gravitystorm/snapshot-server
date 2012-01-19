aab566a4efa75d1672e0d235141728db9fb0cb0e
Andy Allan <andy@gravitystorm.co.uk>
2012-01-19 16:44:37 +0000

    Disable registrations.

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -20,8 +20,8 @@
 class Admin < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :token_authenticatable, :encryptable, :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+  # :registerable, :recoverable
+  devise :database_authenticatable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me
