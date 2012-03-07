81ef4359a647a07dca24f09760b26494786bf783
Andy Allan <andy@gravitystorm.co.uk>
2012-03-07 12:26:22 +0000

    Ensure that the project nodes created have the right project_id

--- spec/factories/project_way_nodes.rb
+++ spec/factories/project_way_nodes.rb
@@ -13,7 +13,7 @@ FactoryGirl.define do
   factory :project_way_node do
     project_id 1
     sequence(:sequence_id) { |n| n}
-    node { FactoryGirl.create(:project_node) }
+    node { FactoryGirl.create(:project_node, :project_id => project_id) }
     way { FactoryGirl.create(:project_way) }
   end
 end
