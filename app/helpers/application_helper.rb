35ef0986de6aeedc8df740c1bd40ad3ec05ce101
Andy Allan <andy@gravitystorm.co.uk>
2012-06-20 12:08:21 +0100

    Refactor the status labels into a helper.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,21 @@
 module ApplicationHelper
+
+  def status_label(entity)
+    default = case entity.class.name
+      when "ProjectNode" then NODE_STATUS_DEFAULT
+      when "ProjectWay" then WAY_STATUS_DEFAULT
+      when "ProjectRelation" then RELATION_STATUS_DEFAULT
+      else ""
+    end
+
+    if entity.status? && (entity.status != default)
+      content_tag("span", :class => [:label, "label-success"]) do
+        entity.status
+      end
+    else
+      content_tag("span", :class => :label) do
+        default
+      end
+    end
+  end
 end
