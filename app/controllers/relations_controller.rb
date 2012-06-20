5ade28e56b1a5182dbba7c302df9a77d697ff6e2
Andy Allan <andy@gravitystorm.co.uk>
2012-06-20 12:37:33 +0100

    Show a details page for relations.

--- app/controllers/relations_controller.rb
+++ app/controllers/relations_controller.rb
@@ -1,4 +1,4 @@
-class NodesController < ApplicationController
+class RelationsController < ApplicationController
   def show
     @project = Project.find(params[:project_id])
     @relation = @project.relations.find(params[:id])
