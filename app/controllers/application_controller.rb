d0e08639e848e66f784b7afc7240d18cd1f16b2d
Andy Allan <andy@gravitystorm.co.uk>
2012-11-07 17:52:28 +0000

    Add function for parsing openlayers bbox strings.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -46,4 +46,11 @@ class ApplicationController < ActionController::Base
       render :status => :unauthorized, :text => t(".application.permission_denied")
     end
   end
+
+  # A method to convert an openlayers-format bbox string into an rgeo bbox object
+  def bbox_from_string(string, factory)
+    minlon, minlat, maxlon, maxlat = string.split(",").collect{|i| i.to_f}
+    bbox = RGeo::Cartesian::BoundingBox.new(factory)
+    bbox.add(factory.point(minlon, minlat)).add(factory.point(maxlon, maxlat))
+  end
 end
