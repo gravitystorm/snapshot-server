62ebd52081f82701336e54c7fc28aded5316514f
Andy Allan <andy@gravitystorm.co.uk>
2012-11-07 12:39:34 +0000

    Colour the progress bars based on the percentage completion.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -22,13 +22,21 @@ module ApplicationHelper
   def progress_bar(percent, label)
     content_tag("div", :class => 'row') do
       content_tag("div", :class => 'span6') do
-        content_tag("div", :class => ['progress', 'progress-striped', 'progress-success']) do
+        content_tag("div", :class => ['progress', 'progress-striped', class_for_progress(percent)]) do
           content_tag("div", nil, :class => 'bar', :style => "width: #{percent}%")
         end
       end +
       content_tag("div", :class => 'span6') do
         content_tag("p", label)
       end
+    end
+  end
+
+  def class_for_progress(percent)
+    if percent < 30 then 'progress-danger'
+    elsif percent < 60 then 'progress-warning'
+    elsif percent < 100 then 'progress-success'
+    else 'progress-info'
     end
   end
 end
